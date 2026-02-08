ee969b3d09d6a4a2d6e8a29eab853d709c2f7961
Asim Maqsood <asimmaqsood57@gmail.com>
2026-02-08 09:28:41 +0500

    cors issue resolved

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000' , 'https://linkedin-post-schedular-frontend-9zgjxapzw.vercel.app'],
     credentials: true,
   });
   
